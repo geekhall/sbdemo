ac1537de75c12a5771b847351e5dd1a35f9e7192
geekhall <174821300@qq.com>
2022-02-15 13:28:42 +0800

    test druid datasource

--- src/test/java/cn/geekhall/sbdemo/SbdemoApplicationTests.java
+++ src/test/java/cn/geekhall/sbdemo/SbdemoApplicationTests.java
@@ -1,10 +1,13 @@
 package cn.geekhall.sbdemo;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Slf4j
 @SpringBootTest
@@ -13,12 +16,16 @@ class SbdemoApplicationTests {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 
+	@Autowired
+	DataSource dataSource;
 
 	@Test
 	void contextLoads() {
 //		jdbcTemplate.queryForList("select * from t_department");
 		Long count = jdbcTemplate.queryForObject("select count(*) from t_department", Long.class);
 		System.out.println("Count: " + count);
+
+		System.out.println("数据源类型：" + dataSource.getClass());
 	}
 
 }

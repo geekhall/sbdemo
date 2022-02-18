14695a85fce6ab9202002e4afdc5a072e7297d27
geekhall <174821300@qq.com>
2022-02-18 22:16:56 +0800

    refacter user

--- src/main/java/cn/geekhall/sbdemo/controller/TableController.java
+++ src/main/java/cn/geekhall/sbdemo/controller/TableController.java
@@ -1,11 +1,15 @@
 package cn.geekhall.sbdemo.controller;
 
+import cn.geekhall.sbdemo.bean.User;
 import cn.geekhall.sbdemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * TableController
@@ -24,9 +28,13 @@ public class TableController {
         return "table/basic_table";
     }
 
+    @ResponseBody
     @GetMapping("/dynamic_table")
     public String dynamic_table(Model model) {
-//        userService.getAll();
+        List<User> users = userService.list();
+        model.addAttribute("users", users);
+
         return "table/dynamic_table";
+//        return use rs;
     }
 }
